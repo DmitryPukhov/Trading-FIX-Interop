e8c0910cca01082fdd681b1d8876f0cfecff4a6f
pdg <pdg@dpuhov.msk.mt>
2013-08-02 16:36:46 +0400

    Connection refused error

--- src/trading/app/interop/fix/Main.java
+++ src/trading/app/interop/fix/Main.java
@@ -10,34 +10,80 @@ import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openfast.Context;
 import org.openfast.Message;
+import org.openfast.MessageBlockReader;
+import org.openfast.MessageInputStream;
+import org.openfast.error.FastException;
+import org.openfast.session.Connection;
+import org.openfast.session.FastConnectionException;
+import org.openfast.session.tcp.TcpEndpoint;
 
 import org.openfast.template.MessageTemplate;
 import org.openfast.template.loader.MessageTemplateLoader;
 import org.openfast.template.loader.XMLMessageTemplateLoader;
 
 public class Main {
+    public final static String host = "194.247.133.77";
+    public final static int port = 5001;
 
     /**
      * @param args See command line options added to "options" above.
      */
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws FileNotFoundException, IOException, FastConnectionException {
 
-        // Read message template
+        // Load templates
         InputStream templateSource = new FileInputStream("config//templates.xml");
         MessageTemplateLoader templateLoader = new XMLMessageTemplateLoader();
-        MessageTemplate[] templates = templateLoader.load(templateSource);
-        MessageTemplate tpl = templates[0];
-
+        //MessageTemplate[] templates = templateLoader.load(templateSource);
+        //MessageTemplate tpl = templates[0];
+        Context context = new Context();
+        context.setTemplateRegistry(templateLoader.getTemplateRegistry());  
+        
         // Connect
-        InetAddress group = InetAddress.getByName("194.247.133.77");
-        MulticastSocket socket = new MulticastSocket(5001);
-        socket.joinGroup(group);
-        MulticastClientInputStream inputStream = new MulticastClientInputStream(socket);
 
-        OpenFastInput fastInput = new OpenFastInput("input", inputStream);
-        Message message = fastInput.readMessage();
-        String str = message.getString(0);
+  
+        org.openfast.session.tcp.TcpEndpoint endpoint = new TcpEndpoint(host, port);
+  
+        Connection c  = endpoint.connect();
+
+        final Socket socket = new Socket(host, port);      
+ 
+        // Create input stream
+        MessageInputStream inputStream = new MessageInputStream(socket.getInputStream(), context);
+        inputStream.setBlockReader(new MessageBlockReader(){
+
+            @Override
+            public boolean readBlock(InputStream in) {
+                return true;
+            }
+
+            @Override
+            public void messageRead(InputStream in, Message message) {
+               
+            }
+        });
+   Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                try {
+                    socket.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });     
+        // Message loop
+        while (true) {
+            try {
+                Message message = inputStream.readMessage();
+                System.out.println(message.toString());
+            }
+            catch(final FastException e) {
+                System.err.println(e.getMessage());
+            }
+        }
 
         //FileOutputStream os = new FileOutputStream(outputFile, true)       
 
